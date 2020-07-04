dc4c10b5c5438407f7753710360a0567beef8d7a
Taufiq Mohammed <taufiqmmhd@gmail.com>
2020-07-04 10:26:43 +0800

    [WIP] feat: freehand draw lines on screen
    Work in progress because line drawing behaviour is hardcoded.
    
    What i mean by hardcoded is that the normally when the user first clicks
    on the canvas, a new line is created and when they then drag their
    cursor, the position of those drag events are added onto that line and
    rendered to the user.
    
    Right now, whenever the user first clicks down, a new line is created.
    However when they drag their mouse, we add those mouse event positions
    to only 1 line and the rest are left dangling.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from "react";
+import { Stage, Layer, Rect, Text } from "react-konva";
+import Konva from "konva";
+import "./App.css";
 
+function ColoredRect() {
+  const [color, setColor] = useState("green");
+  return (
+    <Rect
+      onClick={() => setColor(Konva.Util.getRandomColor())}
+      width={30}
+      height={30}
+      fill={color}
+      draggable
+    />
+  );
+}
 function App() {
+  const stageRef = useRef(null);
+  const layerRef = useRef(null);
+  const [isPainting, setIsPainting] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <h1>Hello</h1>
+        <Stage
+          width={window.innerWidth}
+          height={window.innerHeight}
+          ref={stageRef}
+          onMouseDown={() => {
+            setIsPainting(true);
+            const startingPos = stageRef.current.getPointerPosition();
+            const line = new Konva.Line({
+              stroke: "white",
+              strokeWidth: 5,
+              globalCompositeOperation: 'source-over', // TODO: find out what this is
+              points: [startingPos.x, startingPos.y],
+            });
+            layerRef.current.add(line);
+          }}
+          onMouseUp={() => setIsPainting(false)}
+          onMouseMove={({ evt }) => {
+            if (isPainting) {
+              const line = layerRef.current.children[1] // TODO: refer to new line everytime mouse is downed and not to 1 single line
+              const newPoints = line.points().concat([evt.layerX, evt.layerY])
+              line.points(newPoints)
+              layerRef.current.batchDraw()
+            }
+          }}
         >
-          Learn React
-        </a>
+          <Layer ref={layerRef}>
+            <ColoredRect />
+          </Layer>
+        </Stage>
       </header>
     </div>
   );
